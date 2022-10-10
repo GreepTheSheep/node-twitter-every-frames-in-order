d1a1d89f337ea6a641fc997e5f1437b9e9b2cf34
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2022-10-10 12:47:03 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,4 +4,4 @@ base=$(basename $(pwd))
 
 docker stop fio-$base
 docker build -t frameinorder-$base .
-docker start --rm --name  fio-$base -d frameinorder-$base
+docker run --rm --name fio-$base -d frameinorder-$base
