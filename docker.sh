fd83e0c8b15c15a6445a3103d7354fea8b8e63ec
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2022-10-10 13:35:39 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,5 +4,6 @@ base=$(basename $(pwd))
 port=$(grep -Po 'API_PORT=([0-9]+)' .env | cut -d= -f2)
 
 docker stop fio-$base | true
+docker rm -v $(docker ps -a -q -f status=exited) | true
 docker build -t frameinorder-$base .
-docker run --rm --name fio-$base --restart unless-stopped -d -v $(pwd)/cache:/server/frameinorder/cache -p $port:$port frameinorder-$base
+docker run --name fio-$base --restart unless-stopped -d -v $(pwd)/cache:/server/frameinorder/cache -p $port:$port frameinorder-$base
